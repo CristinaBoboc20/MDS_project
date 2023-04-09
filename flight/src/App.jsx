16dcfd2b4aac0bce65084277a87da5a2e5a589fc
GiuliaAntonia <antoniastate13@gmail.com>
2023-04-09 21:19:21 +0300

    footer commit

---
+++ flight/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Components/Footer/Footer'
+import Subscribers from './Components/Subscribe/Subscribe'
+
+const App = () => {
+    return (
+        <div>
+            <Subscribe/>
+            <Footer/>
+        </div>
+    )
+}
+export default App
