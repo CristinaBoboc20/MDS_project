053875fd46d9c1b336670d8f8b6d898aa6e75d9e
CristinaBoboc20 <cristinaboboc26@yahoo.com>
2023-06-19 23:04:15 +0300

    update files

--- flight/src/index.js
+++ flight/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import Part2Auth from './components/Part2Auth';
 import reportWebVitals from './reportWebVitals';
 
-const domain='dev-syp5l8t5nlchbz2i.us.auth0.com'
-const clientId='nseIDHWgvvSuNsaAjeN0a2wLW3TRQiQe'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri= {window.location.origin}
-    >
-      <App/>
-    </Auth0Provider>
+   <Part2Auth/>
   </React.StrictMode>
 );
  
